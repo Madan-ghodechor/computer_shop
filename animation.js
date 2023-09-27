c6c50f61303207b28f2e22110b66e5ce2b1f769e
Madan-ghodechor <madanghodechor6232@gmail.com>
2023-09-27 17:28:53 +0530

    aos librari added

--- animation.js
+++ animation.js
@@ -1,66 +1,89 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
-    anime({
-        targets:'.fade',
-        translateX:[-100, 0],
-        easing:'easeOutExpo',
-        duration:5000,
-        delay:500,
-        opacity:[0,1]
-    })
-    anime({
-        targets:'.bottomUp',
-        translateY: [70,0],
-        easing:'easeOutExpo',
-        duration:1300,
-        delay:(el, i) => {
-            return 400 + 200 * i;
-        },
-        opacity:[0,1]
-    })
-    anime({
-      targets:'.topDown',
-      translateY: [-20,0],
-      easing:'easeInOutQuad',
-      duration:700,
-      delay:anime.stagger(200, {start: 500}),
-      opacity:[0,1]
-    })
-    anime({
-      targets:'.leftRight',
-      translateX:[-100, 0],
-      easing:'easeOutExpo',
-      duration:5000,
-      delay:500,
-      opacity:[0,1]
+  anime({
+    targets: '.fade',
+    translateX: [-100, 0],
+    easing: 'easeOutExpo',
+    duration: 5000,
+    delay: 500,
+    opacity: [0, 1]
   })
   anime({
-    targets:'.Rightleft',
-    translateX:[100, 0],
-    easing:'easeOutExpo',
-    duration:5000,
-    delay:500,
-    opacity:[0,1]
-})
-anime({
-  targets:'.call',
-  translateY: [1000,0],
-  easing:'easeOutExpo',
-  duration:40,
-  delay:anime.stagger(14, {start: 200}),
-  opacity:[0,1]
-})
-  
+    targets: '.bottomUp',
+    translateY: [70, 0],
+    easing: 'easeOutExpo',
+    duration: 1300,
+    delay: (el, i) => {
+      return 400 + 200 * i;
+    },
+    opacity: [0, 1]
+  })
+  anime({
+    targets: '.topDown',
+    translateY: [-20, 0],
+    easing: 'easeInOutQuad',
+    duration: 700,
+    delay: anime.stagger(200, { start: 500 }),
+    opacity: [0, 1]
+  })
+  anime({
+    targets: '.leftRight',
+    translateX: [-100, 0],
+    easing: 'easeOutExpo',
+    duration: 5000,
+    delay: 500,
+    opacity: [0, 1]
+  })
+  anime({
+    targets: '.Rightleft',
+    translateX: [100, 0],
+    easing: 'easeOutExpo',
+    duration: 5000,
+    delay: 500,
+    opacity: [0, 1]
+  })
+  anime({
+    targets: '.call',
+    translateY: [1000, 0],
+    easing: 'easeOutExpo',
+    duration: 40,
+    delay: anime.stagger(14, { start: 200 }),
+    opacity: [0, 1]
+  })
+
 })
 
-function clickme()
+function bar()
 {
   anime({
-    targets:'.clickEffect',
-    translateY: [-20,0],
-    easing:'easeInOutQuad',
-    duration:300,
-    delay:anime.stagger(20, {start: 300}),
-    opacity:[0,1]
+    targets: '.call',
+    translateY: [1000, 0],
+    easing: 'easeOutExpo',
+    duration: 40,
+    delay: anime.stagger(14, { start: 200 }),
+    opacity: [0, 1]
   })
 }
+function clickme() {
+  anime({
+    targets: '.clickEffect',
+    translateY: [-20, 0],
+    easing: 'easeInOutQuad',
+    duration: 300,
+    delay: anime.stagger(20, { start: 300 }),
+    opacity: [0, 1]
+  })
+}
+
+const obserer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('')
+
+    }
+    else {
+      entry.target.bar()
+    }
+  })
+})
--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-function openNav()
-{
+function openNav() {
     anime({
-        targets:'.RightNavleft',
-        translateX:[200, 0],
-        easing:'easeInOutQuad',
-        duration:400,
-        delay:0,
-        opacity:[0,1]
-      })
+        targets: '.RightNavleft',
+        translateX: [200, 0],
+        easing: 'easeInOutQuad',
+        duration: 400,
+        delay: 0,
+        opacity: [0, 1]
+    })
 
     var target = document.getElementById("navbarSupportedContent");
     target.removeAttribute("class");
-    target.setAttribute("class","sideNavComes RightNavleft ");
+    target.setAttribute("class", "sideNavComes RightNavleft ");
     // document.getElementById('navbarSupportedContent').style.display="inline-block"
 }
-function closeNav()
-{
+function closeNav() {
     var target = document.getElementById("navbarSupportedContent");
-    target.removeAttribute("class","sideNavComes");
-    target.setAttribute("class","collapse navbar-collapse RightNavleft")
+    target.removeAttribute("class", "sideNavComes");
+    target.setAttribute("class", "collapse navbar-collapse RightNavleft")
     // document.getElementById('navbarSupportedContent').style.display="inline-block"
 }
